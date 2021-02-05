32e505673dce91069305cda67d7e4dc6c211135e
lovasszabina <lovasszabina@localhost>
2021-02-05 11:35:48 +0100

    init spring boot app

---
+++ src/main/java/com/newproject/entities/Measurements.java
@@ -0,0 +1,6 @@
+package com.newproject.entities;
+
+public class Measurements {
+
+	
+}
---
+++ src/main/java/com/newproject/entities/Tools.java
@@ -0,0 +1,11 @@
+package com.newproject.entities;
+
+import lombok.Data;
+
+@Data
+public class Tools {
+	
+	private Long id;
+	private String name;
+	private Measurements measurements;
+}
